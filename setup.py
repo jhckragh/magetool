265f674d4acb1b65a42b173f2c4ab08d25aecbc3
Jacob Kragh <jhckragh@gmail.com>
2009-09-20 12:16:38 +0200

    Updated setup script.

--- setup.py
+++ setup.py
@@ -7,9 +7,28 @@ setup(name="Magetool",
       description="CLI tool to automate repetitive tasks in Mage development.",
       author="Jacob Kragh",
       author_email="jhckragh@gmail.com",
-      license="BSD",
+      url="http://www.example.com/",
       packages=["magetool", "magetool.commands", "magetool.libraries",
                 "magetool.templates"],
-      package_dir={"magetool": "src/magetool"},
-      scripts=["src/magetool/magetool"],
+      scripts=["magetool/magetool"],
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 2.6",
+                   "License :: OSI Approved :: BSD License",
+                   "Operating System :: OS Independent",
+                   "Development Status :: 3 - Alpha",
+                   "Environment :: Console",
+                   "Intended Audience :: Developers",
+                   "Topic :: Software Development",
+                   "Topic :: Software Development :: Code Generators"],
+      long_description="""\
+CLI utility for automating common tasks in Mage module development
+------------------------------------------------------------------
+
+Supports
+ - Creating and activating modules
+ - Creating and registering controllers, blocks, helpers, and models
+ - Creating block/helper/model overrides
+
+This program requires lxml.
+"""
       )
