50bd7c1bfb2dc7a9d730dcf9dee1e73e503648db
Jacob Kragh <jhckragh@gmail.com>
2009-10-24 12:59:01 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="magetool",
-      version="0.1.4",
+      version="0.1.5",
       description="CLI tool to automate repetitive tasks in Mage development.",
       author="Jacob Kragh",
       author_email="jhckragh@gmail.com",
